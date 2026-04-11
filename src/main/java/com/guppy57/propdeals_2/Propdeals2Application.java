511299bc8c9ebf4a184e5a43b11349bcf577477a
Armaan Gupta <armaan@guppy.im>
2026-04-11 13:57:31 -0500

    init: spring boot project

---
+++ src/main/java/com/guppy57/propdeals_2/Propdeals2Application.java
@@ -0,0 +1,13 @@
+package com.guppy57.propdeals_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Propdeals2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Propdeals2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/guppy57/propdeals_2/Propdeals2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guppy57.propdeals_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Propdeals2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
